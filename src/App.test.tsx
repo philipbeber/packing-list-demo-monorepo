b88f16e5da0665d4550812cf652d54864cd15a1e
Philip Beber <pbeber@fuze.com>
2020-10-13 07:08:44 -0700

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { createStore, Store } from "redux";
+import rootReducer, { AppState } from "./redux/reducers/rootReducer";
+import { AppActions } from "./redux/actions";
+import { Provider } from "react-redux";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const store: Store<AppState, AppActions> = createStore(rootReducer);
+
+test("renders learn react link", () => {
+  const app = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = app.getAllByText(/Create a camp/i);
+  expect(linkElement.length).toBe(2);
 });
