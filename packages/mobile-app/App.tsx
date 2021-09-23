1f63bdbcdb537670ea2e370f78afa46e560ecf8f
Philip Beber <philip.beber@gmail.com>
2021-09-22 21:36:51 -0700

    Add redux-persist

--- packages/mobile-app/App.tsx
+++ packages/mobile-app/App.tsx
@@ -1,24 +1,31 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 import { Provider } from "react-redux";
-import { store } from "packing-list-shared"
+import { createStore } from "packing-list-shared";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const { store, persistor } = createStore(AsyncStorage);
 
 export default function App() {
-  return <Provider store={store}>
-    <RootUI />
-  </Provider>
-
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RootUI />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 function RootUI() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
 
   if (!isLoadingComplete) {
     return null;
--- packages/shared/lib/redux/store/index.ts
+++ packages/shared/lib/redux/store/index.ts
@@ -1,56 +1,51 @@
-// import * as _ from "lodash";
-import { configureStore } from "@reduxjs/toolkit";
 import campReducer from "../reducers/campReducer";
 import userReducer from "../reducers/userReducer";
-// const version = "0.2";
+import { combineReducers } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    camp: campReducer,
-  },
-  // preloadedState: loadState(),
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { Storage } from "redux-persist";
+
+const rootReducer = combineReducers({
+  camp: campReducer,
+  user: userReducer,
 });
 
+export function createStore(storage: Storage) {
+  const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+  };
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  });
+
+  const persistor = persistStore(store);
+  return {
+    store,
+    persistor,
+  };
+}
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type AppState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
-
-// interface FrozenState {
-//   version: string;
-//   state: {
-//     user: ReturnType<typeof userReducer>;
-//     camp: ReturnType<typeof campReducer>;
-//   };
-// }
-
-// store.subscribe(
-//   _.throttle(() => {
-//     try {
-//       const frozeState: FrozenState = {
-//         version,
-//         state: store.getState(),
-//       };
-//       const serializedState = JSON.stringify(frozeState);
-//       localStorage.setItem("state", serializedState);
-//     } catch {
-//       // ignore write errors
-//     }
-//   }, 1000)
-// );
-
-// function loadState() {
-//   try {
-//     const serializedState = localStorage.getItem("state");
-//     if (serializedState) {
-//       const frozeState = JSON.parse(serializedState) as FrozenState;
-//       if (frozeState.version === version) {
-//         return frozeState.state;
-//       }
-//     }
-//   } catch (err) {
-//     console.warn(err);
-//   }
-//   return undefined;
-// }
+type StoreType = ReturnType<typeof createStore>["store"]
+export type AppState = ReturnType<StoreType["getState"]>
+export type AppDispatch = StoreType["dispatch"];
